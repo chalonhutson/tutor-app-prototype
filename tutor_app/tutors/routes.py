a89532844523f415e78efdb4129fd20334cca63f
Chalon Hutson <chalonhutson@gmail.com>
2023-04-08 19:08:24 -0600

    Updated functionality and css of tutors page and navbar.

--- tutor_app/tutors/routes.py
+++ tutor_app/tutors/routes.py
@@ -3,20 +3,21 @@ from flask import Blueprint, render_template
 tutors = Blueprint("tutors", __name__, url_prefix="/tutors")
 
 class Tutor:
-    def __init__(self, name, img_url, subjects):
+    def __init__(self, name, img_url, subjects, rating):
         self.name = name
         self.img_url = img_url
         self.subjects = subjects
+        self.rating = rating
 
 tutors_list = [
-    Tutor("Emma Thompson", "tutor_1.png", ["math", "history"]),
-    Tutor("Sophia Rodriguez", "tutor_2.png", ["astronomy", "physics"]),
-    Tutor("Matthew Brown", "tutor_3.png", ["history", "social studies", "civics"]),
-    Tutor("Alexander Kim", "tutor_4.png", ["math", "algebra", "calculus"]),
-    Tutor("Christopher Chen", "tutor_5.png", ["civics", "history", "government"])
+    Tutor("Emma Thompson", "tutor_1.png", ["math", "history"], 4.5),
+    Tutor("Sophia Rodriguez", "tutor_2.png", ["astronomy", "physics"], 4.9),
+    Tutor("Matthew Brown", "tutor_3.png", ["history", "social studies", "civics"], 4.7),
+    Tutor("Alexander Kim", "tutor_4.png", ["math", "algebra", "calculus"], 4.8),
+    Tutor("Christopher Chen", "tutor_5.png", ["civics", "history", "government"], 4.5)
 ]
 
 @tutors.route("/")
 def tutors_page():
-    # tutors_list = tutors_list
-    return render_template("tutors.html", tutors_list=tutors_list)
+    subjects = ["Astronomy", "Physics", "Civics", "History", "Algebra", "Calculus"]
+    return render_template("tutors.html", tutors_list=tutors_list, subjects=subjects)
